e4d957fd3a67eaa3f0d353550fc7fc123ef5af46
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-11-23 13:12:34 -0600

    Update example and test file in tests

--- frontend/tests/prueba.spec.ts
+++ frontend/tests/prueba.spec.ts
@@ -1,54 +1,54 @@
-import { expect, test } from '@playwright/test';
+// import { expect, test } from '@playwright/test';
 
-// test('Search click', async ({ page }) => {
-//   await page.goto('https://aguascalientes.tecnm.mx/');
+/* test('Search click', async ({ page }) => {
+  await page.goto('https://aguascalientes.tecnm.mx/');
 
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/TecNM campus Aguascalientes/);
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/TecNM campus Aguascalientes/);
 
-//   // Wait for the page to load completely
-//   await page.waitForLoadState('networkidle');
-//   const emailLink = page.locator('a[href*="login.mricosoftonline.com"][title]="Correo Institucional"');
+  // Wait for the page to load completely
+  await page.waitForLoadState('networkidle');
+  const emailLink = page.locator('a[href*="login.mricosoftonline.com"][title]="Correo Institucional"');
 
-//   // Verify the link exists and is visible
-//   await expect(emailLink).toBeVisible();
+  // Verify the link exists and is visible
+  await expect(emailLink).toBeVisible();
 
-//   // Since the link opens in a new tab, we need to handle that
-//   const [newPage] = await Promise.all([
-//     page.waitForEvent('popup'),
-//     emailLink.click(),
-//   ]);
+  // Since the link opens in a new tab, we need to handle that
+  const [newPage] = await Promise.all([
+    page.waitForEvent('popup'),
+    emailLink.click(),
+  ]);
 
-//   // Verify the new page url
-//   expect(newPage.url()).toContain(/.*login\.microsoftonline\.com/);
-//   await newPage.pause();
-// });
+  // Verify the new page url
+  expect(newPage.url()).toContain(/.*login\.microsoftonline\.com/);
+  await newPage.pause();
+}); */
 
-// test('search click', async ({ page }) => {
-//   await page.goto('https://amazon.com.mx/');
-//   await expect(page).toHaveTitle(/Amazon.com.mx: Precios bajos - Envío rápido - Millones de productos/);
-//   // await page.waitForLoadState('networkidle');
-//   const searchInput = page.locator("input[id='twotabsearchtextbox']")
-//   await expect(searchInput).toBeVisible();
+/* test('search click', async ({ page }) => {
+  await page.goto('https://amazon.com.mx/');
+  await expect(page).toHaveTitle(/Amazon.com.mx: Precios bajos - Envío rápido - Millones de productos/);
+  // await page.waitForLoadState('networkidle');
+  const searchInput = page.locator("input[id='twotabsearchtextbox']")
+  await expect(searchInput).toBeVisible();
 
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/TecNM campus Aguascalientes/);
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/TecNM campus Aguascalientes/);
 
-//   await page.waitForLoadState('networkidle');
-//   const emailLink = page.locator('a[href*="login.microsoftonline.com"][title="Correo]')
+  await page.waitForLoadState('networkidle');
+  const emailLink = page.locator('a[href*="login.microsoftonline.com"][title="Correo]')
 
-//   await expect(emailLink).toBeVisible();
-//   //Sice the link opens in a new tab, we need to handle that
-//   const [newPage] = await Promise.all([
-//     page.waitForEvent('popup'),
-//     emailLink.click()
-//   ])
+  await expect(emailLink).toBeVisible();
+  //Sice the link opens in a new tab, we need to handle that
+  const [newPage] = await Promise.all([
+    page.waitForEvent('popup'),
+    emailLink.click()
+  ])
 
-//   await expect(newPage).toHaveURL(/.*login\.microsoftonline\.com/);
-//   await page.pause()
-// });
+  await expect(newPage).toHaveURL(/.*login\.microsoftonline\.com/);
+  await page.pause()
+}); */
 
-test('search', async ({ page }) => {
+/* test('search', async ({ page }) => {
   await page.goto('https://amazon.com.mx/');
   await expect(page).toHaveTitle(/Amazon.com.mx: Precios bajos - Envío rápido - Millones de productos/);
   
@@ -66,9 +66,9 @@ test('search', async ({ page }) => {
   await expect(first).toBeVisible();
 
   await page.pause();
-});
+}); */
 
-test('search click', async ({ page }) => {
+/* test('search click 2', async ({ page }) => {
   await page.goto('https://www.amazon.com.mx/');
 
   // Expect a title "to contain" a substring.
@@ -88,4 +88,4 @@ test('search click', async ({ page }) => {
   await first.locator("span[class='a-size-base-plus a-color-base a-text-normal']").click();
 
   await page.pause()
-});
+}); */
